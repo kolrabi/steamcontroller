3031ab1dd3d140378e44981b5d511895653a3e9c
ltjax <marius.elvert@googlemail.com>
2020-04-16 18:11:38 +0200

    travis: install libudev

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@ set -x
 if [[ "$(uname -s)" == 'Linux' ]]; then
     # Also install -multilib to cross-compile from x86-64 to x86
     # https://stackoverflow.com/questions/4643197/missing-include-bits-cconfig-h-when-cross-compiling-64-bit-program-on-32-bit
-    sudo apt-get install gcc-${GCC_VERSION} g++-${GCC_VERSION} gcc-${GCC_VERSION}-multilib g++-${GCC_VERSION}-multilib
+    sudo apt-get install gcc-${GCC_VERSION} g++-${GCC_VERSION} gcc-${GCC_VERSION}-multilib g++-${GCC_VERSION}-multilib libudev-dev 
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
     sudo update-alternatives --config gcc
     python3 --version
